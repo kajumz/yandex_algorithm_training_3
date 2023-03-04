ebe4716d0fc702e2ef9ec174ac527bb876de3617
kajumz <57329943+kajumz@users.noreply.github.com>
2023-03-04 16:29:39 +0300

    Create 22.climbing_stairs.cpp

---
+++ 22.climbing_stairs.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+ 
+// A recursive function used by countWays
+int countWaysUtil(int n, int m)
+{
+    int res[n];
+    res[0] = 1;
+    res[1] = 1;
+     
+    for(int i = 2; i < n; i++)
+    {
+       res[i] = 0;
+        
+       for(int j = 1; j <= m && j <= i; j++)
+          res[i] += res[i - j];
+    }
+    return res[n - 1];
+}
+
+int main()
+{
+    int s,m;
+	cin>>s>>m;
+     
+    cout << countWaysUtil(s, m);
+          
+    return 0;
+}
