8907f2c034d3b95e19d8c57a19b40850c6d97d9a
kajumz <57329943+kajumz@users.noreply.github.com>
2023-03-05 15:32:08 +0300

    Create 21. three ones consecutive.py

---
+++ 21. three ones consecutive.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+def resh(n):
+    if n==0: return 1
+    if n==1: return 2
+    if n==2: return 4
+    dp=[0]*(n+1)
+    dp[0]=1
+    dp[1]=2
+    dp[2]=4
+    for i in range(3,n+1):
+        dp[i] = dp[i-1]+dp[i-2]+dp[i-3]
+    return dp[n]
+print(resh(n))
